99674990fa37cf125a4c124b6d3495dade0b636a
Daniel Chinedu <chinedudaniel7@gmail.com>
2016-07-24 16:58:16 +0100

    Add documentation for the various folders in the node starter kit (#1)
    * Complete initial documentation for folder structure
    
    * Complete documentation for the config folder
    
    * Add documentation for controllers, migrations and models
    
    * Add documentation for event handlers, kafka producer, mock server and server
    
    * Version 1.0.0 of spyro completed

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": 0,
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    },
+    "globals": {
+      "process": 0,
+      "__dirname": 0
+    }
+};
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Spyro
 
-Spyro is a simple command line utility that helps to provide documentation and a few other utilities for the micro service starter kit(s) provided in andela.
+Spyro is a simple command line utility that helps to provide documentation and a few other utilities for the micro service
+starter kit(s) provided in andela.
